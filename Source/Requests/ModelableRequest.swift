00676751791856ea8678577dcf0a9c512d9ebad7
Asio Otus <asio.otus.verus@icloud.com>
2021-01-28 18:57:17 +0200

    Add initialisers to modellable request and response

--- Source/Requests/ModelableRequest.swift
+++ Source/Requests/ModelableRequest.swift
@@ -2,6 +2,10 @@ public protocol ModelableRequest: Request {
 	associatedtype Model: RequestModel
 	
 	var model: Model { get }
+	
+	init (_ model: Model)
 }
 
-public protocol RequestModel: Encodable { }
+public protocol RequestModel: Encodable {
+	
+}
--- Source/Requests/ModelableResponse.swift
+++ Source/Requests/ModelableResponse.swift
@@ -2,6 +2,8 @@ public protocol ModelableResponse: Response {
 	associatedtype Model: ResponseModel
 	
 	var model: Model { get }
+	
+	init (_ model: Model)
 }
 
 public protocol ResponseModel: Decodable { }
