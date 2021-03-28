3517986f6256eb1629d9235a5d22b4da21a65911
Asio Otus <asio.otus.verus@icloud.com>
2021-03-28 12:17:23 +0300

    Hotfix

--- Source/RequestsProtocols/ModelableResponse.swift
+++ Source/RequestsProtocols/ModelableResponse.swift
@@ -2,8 +2,6 @@ public protocol ModelableResponse: Response {
 	associatedtype Model: ResponseModel
 	
 	var model: Model { get }
-	
-	init (_ model: Model)
 }
 
 public protocol ResponseModel: Decodable { }
