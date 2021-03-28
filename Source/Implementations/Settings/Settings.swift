eb0af559563c01fed5af03954d58154b628cc84b
Asio Otus <asio.otus.verus@icloud.com>
2021-03-28 13:07:50 +0300

    Fix BaseNetworkUtil Settings

--- Source/Implementations/Settings/Settings.swift
+++ Source/Implementations/Settings/Settings.swift
@@ -5,18 +5,11 @@ public var globalSettings = Settings()
 public struct Settings {
 	public static var global: Self { globalSettings }
 	
-	public let parent: (() -> Settings)?
-	
-	@DerivedSetting public var controllers: Controllers
-	
-	public init (parent: @escaping @autoclosure () -> Self = .global, controllers: Setting<Controllers>) {
-		self.parent = parent
-		self._controllers = .init(controllers.derive(from: parent().controllers))
-	}
+	public var controllers: Controllers
 	
 	public init (controllers: Controllers = .init()) {
 		self.parent = nil
-		self._controllers = .init(.value(controllers))
+		self.controllers = controllers
 	}
 }
 
@@ -25,22 +18,11 @@ public struct Settings {
 extension Settings {
 	public struct Controllers {
 		public static var global: Self { globalSettings.controllers }
-		
-		public let parent: (() -> Self)?
-		
-		@DerivedSetting public var logging: Logging
-		
-		public init (
-			parent: @escaping @autoclosure () -> Self = .global,
-			logging: Setting<Logging> = .copy
-		) {
-			self.parent = parent
-			self._logging = .init(logging.derive(from: parent().logging))
-		}
+				
+		public var logging: Logging
 		
 		public init (logging: Logging) {
-			self.parent = nil
-			self._logging = .init(.value(logging))
+			self.logging = .logging
 		}
 	}
 }
@@ -50,22 +32,11 @@ extension Settings {
 extension Settings.Controllers {
 	public struct Logging {
 		public static var global: Self { globalSettings.controllers.logging }
-		
-		public let parent: (() -> Self)?
-		
-		@DerivedSetting public var loggingProvider: BaseNetworkUtilControllersLoggingProvider
-		
-		public init (
-			parent: @escaping @autoclosure () -> Self = .global,
-			loggingProvider: Setting<BaseNetworkUtilControllersLoggingProvider> = .copy
-		) {
-			self.parent = parent
-			self._loggingProvider = .init(loggingProvider.derive(from: parent().loggingProvider))
-		}
+				
+		public var loggingProvider: BaseNetworkUtilControllersLoggingProvider
 		
 		init (loggingProvider: BaseNetworkUtilControllersLoggingProvider = StandardLoggingProvider()) {
-			self.parent = nil
-			self._loggingProvider = .init(.value(loggingProvider))
+			self.loggingProvider = loggingProvider
 		}
 	}
 }
