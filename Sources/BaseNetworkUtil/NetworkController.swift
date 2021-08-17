f7c430eac5501fa3705cd1036ad00a62262cd892
Asio Otus <asio.otus.verus@icloud.com>
2021-08-17 16:31:15 +0300

    Improve utils objects configuration approach

--- Sources/BaseNetworkUtil/NetworkController.swift
+++ Sources/BaseNetworkUtil/NetworkController.swift
@@ -1,25 +1,23 @@
 import Foundation
 import Combine
 
-public struct NetworkController: NetworkControllerProtocol {
+public class NetworkController: NetworkControllerProtocol {
 	public let source: [String]
 	public let label: String
 	
-	public var delegate: NetworkControllerDelegate?
-	public var watchers: [NetworkControllerWatcher]
+	public var delegate: NetworkControllerDelegate? = nil
+	public var watchers: [NetworkControllerWatcher] = []
 	
 	public init (
-		delegate: NetworkControllerDelegate? = nil,
-		watchers: [NetworkControllerWatcher] = [],
 		source: [String] = [],
 		label: String = "\(Info.moduleName).\(NetworkController.self) – \(#file):\(#line) – \(UUID().uuidString)"
 	) {
 		self.source = source + [Info.moduleName, String(describing: NetworkController.self)]
-		self.delegate = delegate
-		self.watchers = watchers
 		self.label = label
 	}
-	
+}
+
+extension NetworkController {
 	public func send <RequestDelegateType: RequestDelegate> (_ requestDelegate: RequestDelegateType) -> AnyPublisher<RequestDelegateType.ContentType, NetworkController.Error> {
 		_send(requestDelegate)
 	}
@@ -31,24 +29,24 @@ public struct NetworkController: NetworkControllerProtocol {
 			.tryMap { (requestDelegate: RequestDelegateType) -> RequestDelegateType.RequestType in
 				try requestDelegate.request(requestInfo)
 			}
-			.handleEvents(receiveOutput: { (request: RequestDelegateType.RequestType) in watchers.forEach{ $0.onUnmodifiedRequest(request, requestInfo) } })
+			.handleEvents(receiveOutput: { (request: RequestDelegateType.RequestType) in self.watchers.forEach{ $0.onUnmodifiedRequest(request, requestInfo) } })
 			.tryMap { (request: RequestDelegateType.RequestType) -> RequestDelegateType.RequestType in
-				try delegate?.request(request, requestInfo) ?? request
+				try self.delegate?.request(request, requestInfo) ?? request
 			}
-			.handleEvents(receiveOutput: { request in watchers.forEach{ $0.onRequest(request, requestInfo) } })
+			.handleEvents(receiveOutput: { request in self.watchers.forEach{ $0.onRequest(request, requestInfo) } })
 			
 			
 			
 			.tryMap { (request: RequestDelegateType.RequestType) -> (URLSession, URLRequest) in
 				try (requestDelegate.urlSession(request, requestInfo), requestDelegate.urlRequest(request, requestInfo))
 			}
-			.handleEvents(receiveOutput: { (urlSession: URLSession, urlRequest: URLRequest) in watchers.forEach{ $0.onUnmodifiedUrlRequest(urlSession, urlRequest, requestInfo) } })
+			.handleEvents(receiveOutput: { (urlSession: URLSession, urlRequest: URLRequest) in self.watchers.forEach{ $0.onUnmodifiedUrlRequest(urlSession, urlRequest, requestInfo) } })
 			.tryMap { (urlSession: URLSession, urlRequest: URLRequest) -> (URLSession, URLRequest) in
-				let urlSession = try delegate?.urlSession(urlSession, requestInfo) ?? urlSession
-				let urlRequest = try delegate?.urlRequest(urlRequest, requestInfo) ?? urlRequest
+				let urlSession = try self.delegate?.urlSession(urlSession, requestInfo) ?? urlSession
+				let urlRequest = try self.delegate?.urlRequest(urlRequest, requestInfo) ?? urlRequest
 				return (urlSession, urlRequest)
 			}
-			.handleEvents(receiveOutput: { (urlSession: URLSession, urlRequest: URLRequest) in watchers.forEach{ $0.onUrlRequest(urlSession, urlRequest, requestInfo) } })
+			.handleEvents(receiveOutput: { (urlSession: URLSession, urlRequest: URLRequest) in self.watchers.forEach{ $0.onUrlRequest(urlSession, urlRequest, requestInfo) } })
 			
 			.mapError { Error(.preprocessingFailure($0)) }
 			
@@ -62,33 +60,33 @@ public struct NetworkController: NetworkControllerProtocol {
 			
 			
 			
-			.handleEvents(receiveOutput: { (data: Data, urlResponse: URLResponse) in watchers.forEach{ $0.onUrlResponse(data, urlResponse, requestInfo) } })
+			.handleEvents(receiveOutput: { (data: Data, urlResponse: URLResponse) in self.watchers.forEach{ $0.onUrlResponse(data, urlResponse, requestInfo) } })
 			.tryMap { (data: Data, urlResponse: URLResponse) -> (Data, URLResponse) in
-				try (delegate?.data(data, requestInfo) ?? data, delegate?.urlResponse(urlResponse, requestInfo) ?? urlResponse)
+				try (self.delegate?.data(data, requestInfo) ?? data, self.delegate?.urlResponse(urlResponse, requestInfo) ?? urlResponse)
 			}
-			.handleEvents(receiveOutput: { (data: Data, urlResponse: URLResponse) in watchers.forEach{ $0.onModifiedUrlResponse(data, urlResponse, requestInfo) } })
+			.handleEvents(receiveOutput: { (data: Data, urlResponse: URLResponse) in self.watchers.forEach{ $0.onModifiedUrlResponse(data, urlResponse, requestInfo) } })
 			
 			
 			
 			.tryMap { (data: Data, urlResponse: URLResponse) -> RequestDelegateType.ResponseType in
 				try requestDelegate.response(data, urlResponse, requestInfo)
 			}
-			.handleEvents(receiveOutput: { (response: RequestDelegateType.ResponseType) in watchers.forEach{ $0.onResponse(response, requestInfo) } })
+			.handleEvents(receiveOutput: { (response: RequestDelegateType.ResponseType) in self.watchers.forEach{ $0.onResponse(response, requestInfo) } })
 			.tryMap { (response: RequestDelegateType.ResponseType) -> RequestDelegateType.ResponseType in
-				try delegate?.response(response, requestInfo) ?? response
+				try self.delegate?.response(response, requestInfo) ?? response
 			}
-			.handleEvents(receiveOutput: { (response: RequestDelegateType.ResponseType) in watchers.forEach{ $0.onModifiedResponse(response, requestInfo) } })
+			.handleEvents(receiveOutput: { (response: RequestDelegateType.ResponseType) in self.watchers.forEach{ $0.onModifiedResponse(response, requestInfo) } })
 			
 			
 			
 			.tryMap { (response: RequestDelegateType.ResponseType) -> RequestDelegateType.ContentType in
 				try requestDelegate.content(response, requestInfo)
 			}
-			.handleEvents(receiveOutput: { (content: RequestDelegateType.ContentType) in watchers.forEach{ $0.onContent(content, requestInfo) } })
+			.handleEvents(receiveOutput: { (content: RequestDelegateType.ContentType) in self.watchers.forEach{ $0.onContent(content, requestInfo) } })
 			.tryMap { (content: RequestDelegateType.ContentType) -> RequestDelegateType.ContentType in
-				try delegate?.content(content, requestInfo) ?? content
+				try self.delegate?.content(content, requestInfo) ?? content
 			}
-			.handleEvents(receiveOutput: { (content: RequestDelegateType.ContentType) in watchers.forEach{ $0.onModifiedContent(content, requestInfo) } })
+			.handleEvents(receiveOutput: { (content: RequestDelegateType.ContentType) in self.watchers.forEach{ $0.onModifiedContent(content, requestInfo) } })
 			
 			
 			
@@ -97,12 +95,29 @@ public struct NetworkController: NetworkControllerProtocol {
 				receiveCompletion: { completion in
 					if case .failure(let error) = completion {
 						requestDelegate.error(error, requestInfo)
-						delegate?.error(error, requestInfo)
-						watchers.forEach{ $0.onError(error, requestInfo) }
+						self.delegate?.error(error, requestInfo)
+						self.watchers.forEach{ $0.onError(error, requestInfo) }
 					}
 				}
 			)
 		
 		return requestPublisher.eraseToAnyPublisher()
+	}
+}
+
+extension NetworkController {
+	func delegate (_ delegate: NetworkControllerDelegate) -> NetworkController {
+		self.delegate = delegate
+		return self
+	}
+	
+	func watcher (_ watcher: NetworkControllerWatcher) -> NetworkController {
+		self.watchers.append(watcher)
+		return self
+	}
+	
+	func watchers (_ watchers: [NetworkControllerWatcher]) -> NetworkController {
+		self.watchers = watchers
+		return self
 	}
 }
